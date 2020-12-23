82b39ce3a6e6315bb6e91dd0f0e2bb694ac23c3d
yilmazayse00 <32266594+yilmazayse00@users.noreply.github.com>
2020-12-23 14:44:43 +0300

    Route added

--- app/src/main/java/com/example/uber/MainActivity.java
+++ app/src/main/java/com/example/uber/MainActivity.java
@@ -1,10 +1,20 @@
 package com.example.uber;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.FirebaseException;
+import com.google.firebase.auth.PhoneAuthCredential;
+import com.google.firebase.auth.PhoneAuthProvider;
+
+import java.util.concurrent.TimeUnit;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,12 +23,49 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-         Button btn = (Button) findViewById(R.id.button);
-    }
+         final Button btnOtp = findViewById(R.id.getOtp);
+         final EditText inputMobile = findViewById(R.id.editTextPhone);
+         final ProgressBar progressBar = findViewById(R.id.progressBar);
 
-    public void on_Click(View view) {
-        Intent intent = new Intent(getApplicationContext(), VerificationActivity.class);
-        startActivity(intent);
-        finish();
+        btnOtp.setOnClickListener(v -> {
+             if (inputMobile.getText().toString().trim().isEmpty()){
+                 Toast.makeText(MainActivity.this, "Geçerli bir telefon numarası giriniz.", Toast.LENGTH_SHORT).show();
+                 return;
+             }
+             progressBar.setVisibility(View.VISIBLE);
+             btnOtp.setVisibility(View.INVISIBLE);
+
+             PhoneAuthProvider.getInstance().verifyPhoneNumber(
+                     "+90" + inputMobile.getText().toString(),
+                     60,
+                     TimeUnit.SECONDS,
+                     MainActivity.this,
+                     new PhoneAuthProvider.OnVerificationStateChangedCallbacks(){
+                         @Override
+                         public void onVerificationCompleted(@NonNull PhoneAuthCredential phoneAuthCredential) {
+                             progressBar.setVisibility(View.GONE);
+                             btnOtp.setVisibility(View.VISIBLE);
+                         }
+
+                         @Override
+                         public void onVerificationFailed(@NonNull FirebaseException e) {
+                             progressBar.setVisibility(View.GONE);
+                             btnOtp.setVisibility(View.VISIBLE);
+                             Toast.makeText(MainActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+                         }
+
+                         @Override
+                         public void onCodeSent(@NonNull String verificationId, @NonNull PhoneAuthProvider.ForceResendingToken forceResendingToken) {
+                             progressBar.setVisibility(View.GONE);
+                             btnOtp.setVisibility(View.VISIBLE);
+                             Intent intent = new Intent(getApplicationContext(), VerificationActivity.class);
+                             intent.putExtra("mobile", inputMobile.getText().toString());
+                             intent.putExtra("verificationId",verificationId);
+                             startActivity(intent);
+                         }
+                     });
+
+         });
+
     }
 }
--- app/src/main/java/com/example/uber/VerificationActivity.java
+++ app/src/main/java/com/example/uber/VerificationActivity.java
@@ -2,13 +2,175 @@ package com.example.uber;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.PhoneAuthCredential;
+import com.google.firebase.auth.PhoneAuthProvider;
 
 public class VerificationActivity extends AppCompatActivity {
+
+    private EditText inputCode1, inputCode2, inputCode3, inputCode4, inputCode5, inputCode6;
+    private String verificationId;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_verification);
+
+        TextView textMobile = findViewById(R.id.textMobile);
+        textMobile.setText(String.format(
+                "+90-%s", getIntent().getStringExtra("mobile")
+        ));
+
+        inputCode1 = findViewById(R.id.code1);
+        inputCode2 = findViewById(R.id.code2);
+        inputCode3 = findViewById(R.id.code3);
+        inputCode4 = findViewById(R.id.code4);
+        inputCode5 = findViewById(R.id.code5);
+        inputCode6 = findViewById(R.id.code6);
+
+        setupOTPInputs();
+
+         final Button buttonVerify =  findViewById(R.id.buttonVerifiy);
+        verificationId = getIntent().getStringExtra("verificationId");
+
+        buttonVerify.setOnClickListener(v -> {
+            if (inputCode1.getText().toString().trim().isEmpty()
+            || inputCode2.getText().toString().trim().isEmpty()
+            || inputCode3.getText().toString().trim().isEmpty()
+            || inputCode4.getText().toString().trim().isEmpty()
+            || inputCode5.getText().toString().trim().isEmpty()
+            || inputCode6.getText().toString().trim().isEmpty()){
+                Toast.makeText(VerificationActivity.this, "Lütfen onay kodunu giriniz.", Toast.LENGTH_SHORT).show();
+                return;
+            }
+            String code =
+                    inputCode1.getText().toString() +
+                    inputCode2.getText().toString() +
+                    inputCode3.getText().toString() +
+                    inputCode4.getText().toString() +
+                    inputCode5.getText().toString() +
+                    inputCode6.getText().toString();
+
+            if (verificationId != null){
+                PhoneAuthCredential phoneAuthCredential = PhoneAuthProvider.getCredential(
+                        verificationId,
+                        code
+                );
+                FirebaseAuth.getInstance().signInWithCredential(phoneAuthCredential)
+                        .addOnCompleteListener(task -> {
+                            if (task.isSuccessful()){
+                                Intent intent = new Intent(getApplicationContext(),CurrentLocationActivity.class);
+                                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                                startActivity(intent);
+                            }else{
+                                Toast.makeText(VerificationActivity.this, "Girilen kod geçersizdir.", Toast.LENGTH_SHORT).show();
+                            }
+                        });
+            }
+        });
+    }
+
+    private void setupOTPInputs() {
+        inputCode1.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if (!s.toString().trim().isEmpty()){
+                    inputCode2.requestFocus();
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+        inputCode2.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if (!s.toString().trim().isEmpty()){
+                    inputCode3.requestFocus();
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+        inputCode3.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if (!s.toString().trim().isEmpty()){
+                    inputCode4.requestFocus();
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+        inputCode4.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if (!s.toString().trim().isEmpty()){
+                    inputCode5.requestFocus();
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+        inputCode5.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if (!s.toString().trim().isEmpty()){
+                    inputCode6.requestFocus();
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
     }
 }
